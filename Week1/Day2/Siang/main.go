8159bed565b48ce56eab74c4e1e12e554b9b63c4
StevenDharmawan <stevenixe123@gmail.com>
2024-02-20 14:55:58 +0700

    Week 1 Day 2 Siang

--- Week1/Day2/Siang/main.go
+++ Week1/Day2/Siang/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	fmt.Println(pembagian(5, 2))
 	fmt.Println(pangkat(25, 2))
+	fmt.Println(factorial(10))
 }
 
 func pembagian(angka1, angka2 float64) float64 {
@@ -16,4 +17,11 @@ func pembagian(angka1, angka2 float64) float64 {
 
 func pangkat(angka1, angka2 float64) float64 {
 	return math.Pow(angka1, angka2)
+}
+
+func factorial(angka int) int {
+	if angka == 0 {
+		return 1
+	}
+	return angka * factorial(angka-1)
 }
