84910b57f5a1a7a5e11116e1ec6b33fa74c8ac08
StevenDharmawan <stevenixe123@gmail.com>
2024-02-28 14:42:09 +0700

    NGC10

--- NGC/NGC10/Nomor2.go
+++ NGC/NGC10/Nomor2.go
@@ -3,29 +3,52 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 )
 
-func InputUser(text string, scanner *bufio.Scanner) string {
+func main() {
+	defer func() {
+		if message := recover(); message != nil {
+			fmt.Println(message)
+		}
+	}()
+	scanner := bufio.NewScanner(os.Stdin)
+	operator := inputUser("Pilih operasi aritmatika:\n>Penjumlahan (+) a\n>Pengurangan (-) b\n> Perkalian (*) c\n> Pembagian (/) d", scanner)
+	angka1 := isInteger(inputUser("Masukkan angka 1: ", scanner))
+	angka2 := isInteger(inputUser("Masukkan angka 2: ", scanner))
+	chooseOperator(operator, angka1, angka2)
+
+}
+
+func inputUser(text string, scanner *bufio.Scanner) string {
 	fmt.Println(text)
 	scanner.Scan()
 	return scanner.Text()
 }
 
-func isInteger(text string) {
-
+func isInteger(text string) int {
+	i, err := strconv.Atoi(text)
+	if err != nil {
+		panic("Input bukan Integer")
+	}
+	return i
 }
 
 func chooseOperator(text string, angka1 int, angka2 int) {
 	switch text {
 	case "a":
-		fmt.Println("one")
+		fmt.Printf("hasil penjumlahan %d dan %d adalah %d\n", angka1, angka2, angka1+angka2)
 	case "b":
-		fmt.Println("two")
+		fmt.Printf("hasil pengurangan %d dan %d adalah %d\n", angka1, angka2, angka1-angka2)
 	case "c":
-		fmt.Println("three")
+		fmt.Printf("hasil perkalian %d dan %d adalah %d\n", angka1, angka2, angka1*angka2)
 	case "d":
-		fmt.Println("four")
+		if angka2 == 0 {
+			panic("Tidak dapat melakukan pembagian 0")
+		}
+		fmt.Printf("hasil pembagian %d dan %d adalah %d\n", angka1, angka2, angka1/angka2)
 	default:
-		panic("Input tidak valid")
+		panic("Input operator tidak valid")
 	}
 }
